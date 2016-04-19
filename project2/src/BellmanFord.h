17083331957b323559df063d80bc3f2d65073d08
DanielFigueira <daniel.figueira@ist.utl.pt>
2016-04-19 16:19:27 +0100

    Bellman-Ford almost ready (check FIXMEs)

--- project2/src/BellmanFord.h
+++ project2/src/BellmanFord.h
@@ -6,21 +6,15 @@ class BellmanFord {
 	
 private:
 
-	static Node *createSource(Graph *graph){
-		Node *source = new Node(0);
-		for (int t = 0; t < graph.getNumberOfNodes(); t++)
-			source.connect(graph.getNodeAt(t), 0);
-	}
-
 	static void initializeSingleSource(Graph *graph, Node *source){
 		source.setCost(0);
 		for (int t = 0; t < graph.getNumberOfNodes(); t++)
-			graph.getNodeAt(t).setCost(NULL);
+			graph.getNodeAt(t).setCost(99999); //FIXME : CLIMITS?
 	}
 	
 	static void relax(Node *u, Node *v, int weight){
-		if (v.getCost() > u.getCost() + weight) {
-			v.setCost(u.getCost() + weight)
+		if (v.getPathCost() > u.getPathCost() + weight) {
+			v.setPathCost(u.getPathCost() + weight)
 			v.setParent(u);
 		}
 	}
@@ -28,14 +22,19 @@ private:
 public:
 	
 	// runs bellman-ford algorithm starting at node with given id
-	static bool run(Graph *graph, int id){
+	static void run(Graph *graph, int id){
+		Node *u:
+		Node *v;
+		int weight;
 		
+		initializeSingleSource(graph, graph.getNodeAt(0));
 		
+		for (int t = 1; t < graph.getNumberOfNodes(); t++){
+			
+			// FIXME: INSERT LOOP HERE
+			// 		RELAX();
+		}
 		
+		// doesn't check for negative loops since there's no need
 	}
-		
-	
-	
-	
-	
 }
